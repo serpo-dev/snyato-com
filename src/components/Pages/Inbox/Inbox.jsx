f80156afc34bc86573f65746f30c39cca07faac4
Sergey <sergey.potapov.2002@mail.ru>
2022-06-04 11:18:39 +0500

    add arrays in chatItems

--- src/components/Pages/Inbox/Inbox.jsx
+++ src/components/Pages/Inbox/Inbox.jsx
@@ -4,14 +4,24 @@ import ChatItem from './ChatItem/ChatItem'
 import SelectedChat from "./SelectedChat/SelectedChat";
 
 const Inbox = () => {
+
+    const inboxData = [
+        { name: 'Serega', id: '1' },
+        { name: 'Lisa', id: '2' },
+        { name: 'Masha', id: '3' },
+        { name: 'Kate', id: '4' },
+        { name: 'Artyom', id: '5' },
+        { name: 'Erich', id: '6' }
+    ]
+
+    const listItems = inboxData.map((object) => {
+        return (<ChatItem name={object.name} id={object.id} />)
+    })
+
     return (
         <div className={stylesheet.messages}>
             <div className={stylesheet.people}>
-                <ChatItem name='Serega' id='1' />
-                <ChatItem name='Lisa' id='2' />
-                <ChatItem name='Masha' id='3' />
-                <ChatItem name='Kate' id='4' />
-                <ChatItem name='Artyom' id='5' />
+                {listItems}
             </div>
             <div className={stylesheet.dialogs}>
                 <div>

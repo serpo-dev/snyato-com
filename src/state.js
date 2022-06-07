f73af7d410f531704d50d9a83d0b38d6805d277d
Sergey <sergey.potapov.2002@mail.ru>
2022-06-07 19:48:15 +0500

    move action type consts in state.js out

--- src/state.js
+++ src/state.js
@@ -1,8 +1,11 @@
 import chats from './redux/data/inboxItems.json';
 
+const ONE_BY_ONE_CHARACHTERS_ENTERING = 'ONE-BY-ONE-CHARACHTERS-ENTERING';
+const ADD_NEW_MESSAGE = 'ADD-NEW-MESSAGE'
 
-export let oneByOneCharachtersEnteringActionCreator = (desiredValue) => ({ type: 'ONE-BY-ONE-CHARACHTERS-ENTERING', desiredValue: desiredValue })
-export let addNewMessageActionCreator = (desiredValue) => ({ type: 'ADD-NEW-MESSAGE', desiredValue: desiredValue })
+
+export let oneByOneCharachtersEnteringActionCreator = (desiredValue) => ({ type: ONE_BY_ONE_CHARACHTERS_ENTERING, desiredValue: desiredValue })
+export let addNewMessageActionCreator = (desiredValue) => ({ type: ADD_NEW_MESSAGE, desiredValue: desiredValue })
 
 export let store = {
     _state: {

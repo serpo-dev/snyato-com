648661ccf645d519850c44ddfefc4065461ffe54
Sergey <sergey.potapov.2002@mail.ru>
2022-06-14 17:37:48 +0500

    convert stateless "Profile" and "Inbox" to the class components

--- src/components/Pages/InboxContainer/Inbox/Inbox.jsx
+++ src/components/Pages/InboxContainer/Inbox/Inbox.jsx
@@ -3,65 +3,74 @@ import React from "react";
 import stylesheet from './Inbox.module.css'
 
 
-const Inbox = (props) => {
+class Inbox extends React.Component {
 
-    // convert html to the js object
+    constructor(props) {
+        super(props);
 
-    let textAreaConverterHTMLtoJS = React.createRef();
+        // load data at the start from db.json
+        if (this.props.p.length === 0) {
+            axios
+                .get("http://localhost:3001/users")
+                .then(response => {
+                    this.props.callSetUsersActionCreator(response.data);
+                });
+        }
 
-    // unpacking props properties
-
-    let p = props.p;
-    let m = props.m;
-    let currentValueOfTextArea = props.currentValueOfTextArea;
-
-    const callOneByOneCharachtersEnteringActionCreator = () => { props.callOneByOneCharachtersEnteringActionCreator(textAreaConverterHTMLtoJS.current.value) };
-    const callAddNewMessageActionCreator = () => { props.callAddNewMessageActionCreator() };
-
-    // the JSX code is to return
-
-    let result = (
-        <div>
-            <div className={stylesheet.messages}>
-                <div className={stylesheet.people}>
-                    {p}
-                </div>
-                <div className={stylesheet.dialogs}>
-                    <div className={stylesheet.textarea}>
-                        <textarea
-                            onChange={callOneByOneCharachtersEnteringActionCreator}
-                            ref={textAreaConverterHTMLtoJS}
-                            placeholder='Enter here your message'
-                            value={currentValueOfTextArea} />
-                        <button onClick={callAddNewMessageActionCreator}>Send</button>
-                    </div>
-                    <div className={stylesheet.mItems}>
-                        <br />
-                        {m}
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-
-    // load data at the start from db.json
-
-    if (p.length === 0) {
-        axios
-            .get("http://localhost:3001/users")
-            .then(response => {
-                props.callSetUsersActionCreator(response.data);
-            });
-        if (m.length === 0) {
+        if (this.props.m.length === 0) {
             axios
                 .get("http://localhost:3001/messages")
                 .then(response => {
-                    props.callSetMessagesActionCreator(response.data);
+                    this.props.callSetMessagesActionCreator(response.data);
                 });
-            return result;
-        } else { return result }
-    } else { return result }
+        }
+    }
 
+    render() {
+
+        // convert html to the js object
+
+        let textAreaConverterHTMLtoJS = React.createRef();
+
+        // unpacking props properties
+
+        let p = this.props.p;
+        let m = this.props.m;
+        let currentValueOfTextArea = this.props.currentValueOfTextArea;
+
+        const callOneByOneCharachtersEnteringActionCreator = () => { this.props.callOneByOneCharachtersEnteringActionCreator(textAreaConverterHTMLtoJS.current.value) };
+        const callAddNewMessageActionCreator = () => { this.props.callAddNewMessageActionCreator() };
+
+        // the JSX code is to return
+
+        return (
+            <div>
+                <div className={stylesheet.messages}>
+                    <div className={stylesheet.people}>
+                        {p}
+                    </div>
+                    <div className={stylesheet.dialogs}>
+                        <div className={stylesheet.textarea}>
+                            <textarea
+                                onChange={callOneByOneCharachtersEnteringActionCreator}
+                                ref={textAreaConverterHTMLtoJS}
+                                placeholder='Enter here your message'
+                                value={currentValueOfTextArea} />
+                            <button onClick={callAddNewMessageActionCreator}>Send</button>
+                        </div>
+                        <div className={stylesheet.mItems}>
+                            <br />
+                            {m}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+
+
+
+
+    }
 }
 
 export default Inbox;
--- src/components/Pages/ProfileContainer/Profile/Profile.jsx
+++ src/components/Pages/ProfileContainer/Profile/Profile.jsx
@@ -3,48 +3,57 @@ import * as axios from 'axios';
 import stylesheet from './Profile.module.css';
 
 
-const Profile = (props) => {
+class Profile extends React.Component {
 
-    // convert HTML object to the JS language
+    constructor(props) {
+        super(props);
 
-    let textAreaConverterHTMLtoJS = React.createRef();
+        // load data at the start from db.json
 
-    // exctract props
+        if (this.props.posts.length === 0) {
+            axios
+                .get("http://localhost:3001/posts")
+                .then(response => {
+                    this.props.callSetPostsActionCreator(response.data);
+                });
+        }
 
-    let posts = props.posts;
-    let currentValueOfTextArea = props.currentValueOfTextArea;
-
-    const callOneByOneCharachtersEnteringActionCreator = () => { props.callOneByOneCharachtersEnteringActionCreator(textAreaConverterHTMLtoJS.current.value) }
-    const callAddNewPostActionCreator = () => { props.callAddNewPostActionCreator() }
-
-    // load data at the start from db.json
-
-    if (posts.length === 0) {
-        axios
-            .get("http://localhost:3001/users")
-            .then(response => {
-                props.callSetPostsActionCreator(response.data);
-            });
     }
 
+    render() {
 
-    return (
-        <div className={stylesheet.Prolile}>
-            <div className={stylesheet.inputBox}>
-                <textarea
-                    onChange={callOneByOneCharachtersEnteringActionCreator}
-                    ref={textAreaConverterHTMLtoJS}
-                    placeholder="What's the new?"
-                    value={currentValueOfTextArea}
-                />
-                <button onClick={callAddNewPostActionCreator}>Publish</button>
+        // convert HTML object to the JS language
+
+        let textAreaConverterHTMLtoJS = React.createRef();
+
+        // exctract props
+
+        let posts = this.props.posts;
+        let currentValueOfTextArea = this.props.currentValueOfTextArea;
+
+        const callOneByOneCharachtersEnteringActionCreator = () => { this.props.callOneByOneCharachtersEnteringActionCreator(textAreaConverterHTMLtoJS.current.value) }
+        const callAddNewPostActionCreator = () => { this.props.callAddNewPostActionCreator() }
+
+        // return JSX
+
+        return (
+            <div className={stylesheet.Prolile}>
+                <div className={stylesheet.inputBox}>
+                    <textarea
+                        onChange={callOneByOneCharachtersEnteringActionCreator}
+                        ref={textAreaConverterHTMLtoJS}
+                        placeholder="What's the new?"
+                        value={currentValueOfTextArea}
+                    />
+                    <button onClick={callAddNewPostActionCreator}>Publish</button>
+                </div>
+                <div>
+                    <br />
+                    {posts}
+                </div>
             </div>
-            <div>
-                <br />
-                {posts}
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Profile;

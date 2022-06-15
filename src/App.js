d486ef7d22980623b6f224c854816b319a622bdd
Sergey <sergey.potapov.2002@mail.ru>
2022-06-15 19:15:22 +0500

    made bodybar css

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import Actionbar from './components/Actionbar/Actionbar'
 import Infobar from './components/Infobar/Infobar'
 import Navbar from './components/Navbar/Navbar'
 import Pages from './components/Pages/Pages'
+import Bodybar from './bodybar.png'
 
 function App(props) {
   return (
-    <div className="appWrapper">
-      <Navbar />
-      <Actionbar />
-      <Pages store={props.store} />
-      <Infobar />
-    </div >
+    <div>
+      <div className='bodybar'>
+        <img src={Bodybar} />
+      </div>
+      <div className='appWrapperContainer'>
+        <div className="appWrapper">
+          <Navbar />
+          <Actionbar />
+          <Pages store={props.store} />
+          <Infobar />
+        </div >
+      </div>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import { Provider } from 'react-redux'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+    <div className='App'>
         <BrowserRouter>
             <Provider store={store}>
                 <App />
             </Provider>
         </BrowserRouter>
+    </div>
 );

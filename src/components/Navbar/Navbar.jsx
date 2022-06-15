0effa1b9d00f2d1f42117be9eb63c3e3c79b0994
Sergey <sergey.potapov.2002@mail.ru>
2022-06-15 13:13:28 +0500

    finish navbar css flex

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,35 +8,33 @@ import addButton from './addButton.png'
 const Navbar = () => {
     return (
         <div className='Navbar'>
-            <div>
-                <ul className={stylesheet.navbar}>
-                    <li>
-                        <NavLink to='/home' className={(navData) => navData.isActive ? stylesheet.activeStateSmallButtons : stylesheet.smallButtons}>
-                            <p>Home</p>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/search' className={(navData) => navData.isActive ? stylesheet.activeStateSmallButtons : stylesheet.smallButtons}>
-                            <p>Discover</p>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/upload' className={stylesheet.addButton}>
-                            <img src={addButton}></img>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/im' className={(navData) => navData.isActive ? stylesheet.activeStateSmallButtons : stylesheet.smallButtons}>
-                            <p>Inbox</p>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/user' className={(navData) => navData.isActive ? stylesheet.activeStateSmallButtons : stylesheet.smallButtons}>
-                            <p>Profile</p>
-                        </NavLink>
-                    </li>
-                </ul>
-            </div>
+            <ul className={stylesheet.navbar}>
+                <li>
+                    <NavLink to='/home' className={(navData) => navData.isActive ? stylesheet.activeStateSmallButtons : stylesheet.smallButtons}>
+                        <p>Home</p>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/search' className={(navData) => navData.isActive ? stylesheet.activeStateSmallButtons : stylesheet.smallButtons}>
+                        <p>Discover</p>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/upload' className={stylesheet.addButton}>
+                        <img src={addButton}></img>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/im' className={(navData) => navData.isActive ? stylesheet.activeStateSmallButtons : stylesheet.smallButtons}>
+                        <p>Inbox</p>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/user' className={(navData) => navData.isActive ? stylesheet.activeStateSmallButtons : stylesheet.smallButtons}>
+                        <p>Profile</p>
+                    </NavLink>
+                </li>
+            </ul>
         </div>
     )
 }

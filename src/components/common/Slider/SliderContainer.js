60c6cc4d637c8516e3a1093f1b5de091f3c2d171
Sergey <sergey.potapov.2002@mail.ru>
2022-06-26 17:33:35 +0500

    promises do not work

--- src/components/common/Slider/SliderContainer.js
+++ src/components/common/Slider/SliderContainer.js
@@ -7,11 +7,17 @@ import * as axios from 'axios'
 
 class SliderContainer extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.connectToggle = true;
+    }
+
     componentDidMount() {
 
         let state = this.props.state;
         let axiosType = this.props.axiosType;
 
+        this.connectToggle = true;
 
         // load data at the start from db.json
 
@@ -26,6 +32,8 @@ class SliderContainer extends React.Component {
                 state.isFetchingToggle(false)
                 state.setPosts(response.data)
             });
+
+        console.log(2)
 
     }
 
@@ -45,7 +53,6 @@ class SliderContainer extends React.Component {
             let endCount = startCount + 3;
 
             let frame = document.querySelector(`.${stylesheet.frame}`)
-
 
             if (frame.style.top === "-450px") {
 
@@ -91,7 +98,7 @@ class SliderContainer extends React.Component {
                         frame.style.top = "-900px"
                         frame.style.transition = null;
                         setTimeout(() => {
-                            frame.style.top = "-450px"
+                            frame.style.top = "-450px";
                             frame.style.transition = "top 0.2s ease-out 0s";
                         }, 50)
                     });
@@ -99,30 +106,50 @@ class SliderContainer extends React.Component {
                 frame.style.top = "0px"
                 frame.style.transition = "top 0.2s ease-out 0s";
                 setTimeout(() => {
-                    frame.style.top = "100px"
+                    frame.style.top = "100px";
                     setTimeout(() => {
-                        frame.style.top = "0px"
+                        frame.style.top = "0px";
+                        state.isFetchingToggle(false);
                     }, 50);
                 }, 50);
 
-                state.isFetchingToggle(false)
+
             }
         }
 
         let sum = state.sum;
         const incrementSum = state.incrementSum;
 
-        const onWheel = (e) => {
-            if (e.deltaY > 0) {
-                incrementSum(1);
-            } else if (e.deltaY < 0) {
-                incrementSum(-1);
-            }
 
-            if (sum === 3) {
-                getNextPost();
-            } else if (sum === -3) {
-                getPreviousPost();
+
+        const onWheel = (e) => {
+
+            if (this.connectToggle === true) {
+                if (e.deltaY > 0) {
+                    incrementSum(1);
+                } else if (e.deltaY < 0) {
+                    incrementSum(-1);
+                }
+
+                if (sum === 3) {
+                    const propmise = new Promise((resolve) => {
+                        getNextPost();
+                        this.connectToggle = false;
+                        resolve(true);
+                    }).then(resolve => {
+                        this.connectToggle = resolve;
+                    })
+
+                } else if (sum === -3) {
+                    const promise = new Promise((resolve) => {
+                        getPreviousPost();
+                        this.connectToggle = false;
+                        resolve(true);
+                    }).then(resolve => {
+                        this.connectToggle = resolve;
+                    })
+
+                }
             }
         }
 
